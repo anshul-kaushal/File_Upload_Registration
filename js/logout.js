5bb6d8e13e37d33d7fe6399d7bdd66f1d4197f39
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2021-07-08 19:53:26 -0400

    Addition of js files

---
+++ js/logout.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    firebase.auth().onAuthStateChanged(function (user) {
+      console.log("user", user);
+      if (user) {
+        firebase.auth().signOut();
+      }else{
+        window.location = "login.html";  
+      }
+    });
+  });
